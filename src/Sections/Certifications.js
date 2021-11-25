b9e1136640b82bc66ae99f7f343d33b1cf70f23c
Developer Terence <tko.developer.02@gmail.com>
2021-11-25 12:39:08 +0800

    Update Projects and Certifications

--- src/Sections/Certifications.js
+++ src/Sections/Certifications.js
@@ -11,12 +11,10 @@ function Certifications() {
             <div class="col-span-10 font-light"> NICF - Containers for deploying and scaling apps </div>
             <div class="col-span-2 grid text-xs font-extralight italic justify-items-end"> Sep. 2019 </div>
 
-            
-
-            <div class="col-span-10 font-bold"> AWS </div>
+            {/* <div class="col-span-10 font-bold"> AWS </div>
             <div class="col-span-2 text-blue-400 text-xs font-light italic justify-items-end content-end"> </div>
             <div class="col-span-10 font-light"> Cloud Practitioner </div>
-            <div class="col-span-2 grid text-xs font-extralight italic justify-items-end"> Nov. 2021 </div>
+            <div class="col-span-2 grid text-xs font-extralight italic justify-items-end"> Nov. 2021 </div> */}
         </div>
       </div>
     );
--- src/Sections/Projects.js
+++ src/Sections/Projects.js
@@ -4,11 +4,13 @@ function Projects() {
     return (
       <div>
             <SectionHeader title="Projects" />
-            <div class="font-bold"> CakeChef </div>
-            <div class="mt-1 font-light"> A generic, configuration based binary file reader/writer. </div>
+            <div class="font-bold"> <a href="https://gitlab.com/tko.developer.02/CakeChef"> CakeChef </a> </div>
 
             <ul class="mb-4 list-disc list-inside font-extralight">
-                <li> Developed the application using Javascript and Node. </li>
+                <li> A generic, configuration based binary file reader/writer built for systems which utilize binary files as a means of information transfer. </li>
+                <li> Developed the application using Javascript and NodeJS. </li>
+                {/* <li> Setup the CICD pipeline using Gitlab and Jenkins. </li>
+                <li> Created documentation using X framework. </li> */}
             </ul>
 
       </div>
