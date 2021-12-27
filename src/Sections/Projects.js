8fd96f14eae21fb482c07765d056c5bcb89c4fa0
Developer Terence <tko.developer.02@gmail.com>
2021-12-27 12:45:52 +0800

    Updated Projects

--- src/Sections/Projects.js
+++ src/Sections/Projects.js
@@ -4,13 +4,15 @@ function Projects() {
     return (
       <div>
             <SectionHeader title="Projects" />
-            <div class="font-bold"> <a href="https://gitlab.com/tko.developer.02/CakeChef"> CakeChef </a> </div>
-
+            <div class="font-bold"> <a href="https://gitlab.com/tko.developer.02/CakeChef"> CakeChef  </a> </div>
+            
             <ul class="mb-4 list-disc list-inside font-extralight">
-                <li> A generic, configuration based binary file reader/writer built for systems which utilize binary files as a means of information transfer. </li>
-                <li> Developed the application using Javascript and NodeJS. </li>
-                {/* <li> Setup the CICD pipeline using Gitlab and Jenkins. </li>
-                <li> Created documentation using X framework. </li> */}
+                <li> An Open Source, 2-in-1, binary file reader/writer. </li>
+                <li> YAML based configuration with support for dynamic configuration (variables, loops, conditional statements). </li>
+                <li> Support for various encoding/decoding formats (endianness, date/string/integer) </li>
+                <li> Convenience utilities (crc32 generator) </li>
+                <li> Built in Javascript and NodeJS. </li>
+                <li> CICD Pipeline using Gitlab CI to run Jest tests. </li>
             </ul>
 
       </div>
