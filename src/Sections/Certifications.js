3f1f854674ab5065241a91730292945dc41af89f
Developer Terence <tko.developer.02@gmail.com>
2022-05-10 19:00:52 +0800

    Update build

--- src/Sections/Certifications.js
+++ src/Sections/Certifications.js
@@ -7,13 +7,13 @@ function Certifications() {
         <div class="mb-2 grid grid-cols-2">
             <div class="font-bold"> AWS Certified Cloud Practitioner </div>
             <div class="grid text-blue-400 text-xs font-light italic justify-items-end content-end"> </div>
-            <div class="uppercase text-xs font-light"> Amazon Web Services (AWS) </div>
+            <div class="font-light"> Amazon Web Services (AWS) </div>
             <div class="grid text-xs font-extralight italic justify-items-end"> May. 2022 - May. 2025 </div>
         </div>
         {/* <div class="mb-2 grid grid-cols-2">
             <div class="font-bold"> AWS Certified Solutions Architect </div>
             <div class="grid text-blue-400 text-xs font-light italic justify-items-end content-end"> </div>
-            <div class="uppercase text-xs font-light"> Amazon Web Services (AWS) </div>
+            <div class="font-light"> Amazon Web Services (AWS) </div>
             <div class="grid text-xs font-extralight italic justify-items-end"> Jun. 2022 - Jun. 2025 </div>
         </div> */}
       </div>
