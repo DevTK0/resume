d2332aea56713319ce9348f271c1948b044c8175
Terence Kong <tko.developer.02@gmail.com>
2022-08-29 20:36:58 +0800

    Update build

--- src/Sections/Certifications.js
+++ src/Sections/Certifications.js
@@ -6,24 +6,18 @@ function Certifications() {
             <SectionHeader title="Certifications" />
             <div class="mb-2 grid grid-cols-2">
                 <div class="font-bold"> AWS Certified Cloud Practitioner </div>
-                <div class="grid text-blue-400 text-xs font-light italic justify-items-end content-end">
-                    {" "}
-                </div>
+                <div class="grid text-blue-400 text-xs font-light italic justify-items-end content-end"></div>
                 <div class="font-light"> Amazon Web Services (AWS) </div>
                 <div class="grid text-xs font-extralight italic justify-items-end">
-                    {" "}
-                    May. 2022 - May. 2025{" "}
+                    May. 2022 - May. 2025
                 </div>
             </div>
             <div class="mb-2 grid grid-cols-2">
                 <div class="font-bold"> AWS Certified Solutions Architect </div>
-                <div class="grid text-blue-400 text-xs font-light italic justify-items-end content-end">
-                    {" "}
-                </div>
+                <div class="grid text-blue-400 text-xs font-light italic justify-items-end content-end"></div>
                 <div class="font-light"> Amazon Web Services (AWS) </div>
                 <div class="grid text-xs font-extralight italic justify-items-end">
-                    {" "}
-                    Aug. 2022 - Aug. 2025{" "}
+                    Aug. 2022 - Aug. 2025
                 </div>
             </div>
         </div>
